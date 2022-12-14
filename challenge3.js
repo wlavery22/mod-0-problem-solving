246d2d5df2eec1724f41d904c0ac994f9a7d02f9
William Lavery <lavery_william@yahoo.com>
2022-12-13 22:14:51 -0500

    Initial commit

---
+++ challenge3.js
@@ -0,0 +1,12 @@
+var friends = ["tara", "steve", "sam", "sandra", "tommy"];
+for (var i = 0; i < friends.length; i++) {
+    if(friends[i].startsWith("t")) {
+    console.log(friends[i]);
+  }
+};
+// Overall goal is to print only the names (elements / strings) that begin with "t". 
+// Pseudocode: We need a for statement that will loop through each (element / string) of the array and return only those that begin with "t".
+// The final solution should return the elements that begin with "t". That's tara and tommy.    
+
+
+
